9b74998b4e6530f3a51ad03247b2d88c5c69f3ac
Adrian Cuadros <adrian@innku.com>
2010-07-01 18:16:19 -0500

    Adds meta where gem dependency

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,9 @@
-class Coutry < ActiveRecord::Base
+class Country < ActiveRecord::Base
   has_many :states
+  
+  def self.country_like(country)
+    where("countries.name #{self.like_statement} ? OR countries.abbr #{self.like_statement} ?",
+          "%#{country}%","%#{country}%")
+  end
+  
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,11 @@
 class State < ActiveRecord::Base
   belongs_to :country
   has_many :cities
+  
+  def self.state_like(state)
+    where("states.name #{self.like_statement} ? OR states.abbr #{self.like_statement} ? OR 
+           states.abbr2 #{self.like_statement} ? OR states.abbrs3 #{self.like_statement} ?",
+          "%#{state}%","%#{state}%","%#{state}%","%#{state}%")
+  end
+  
 end
