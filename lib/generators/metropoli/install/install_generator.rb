a35070252917fdd71d6e699bd7e3319ae5e22c38
Adrian Cuadros <adrian@innku.com>
2010-07-02 11:25:59 -0500

    changes generator structure

---
+++ lib/generators/metropoli/install/install_generator.rb
@@ -0,0 +1,18 @@
+module Metropoli
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      
+      source_root File.expand_path('../templates',__FILE__)
+      
+      def self.next_migration_number(path)
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      end
+      
+      def generate_migration
+        migration_template 'migrate/create_metropoli.rb', 'db/migrate/create_metropoli.rb'
+      end
+      
+    end
+  end
+end
--- lib/generators/metropoli_generator.rb
+++ lib/generators/metropoli_generator.rb
@@ -1,18 +1,2 @@
-module Metropoli
-  module Generators
-    class MetropoliGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
-      
-      source_root File.expand_path('../templates',__FILE__)
-      
-      def self.next_migration_number(path)
-        Time.now.utc.strftime("%Y%m%d%H%M%S")
-      end
-      
-      def generate_migration
-        migration_template 'migrate/create_metropoli.rb', 'db/migrate/create_metropoli.rb'
-      end
-      
-    end
-  end
-end
+#todo require metropoli generator
+#require "generators/metropoli/metropoli/metropoli_generator"
