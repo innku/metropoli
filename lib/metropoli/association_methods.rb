684eee0ffbb27abce3cf7c0ee5847ba0813f7144
Adrian Cuadros <adrian@innku.com>
2010-07-06 11:53:48 -0500

    First attempt at creating a model relation to metropoli

--- lib/metropoli/association_methods.rb
+++ lib/metropoli/association_methods.rb
@@ -6,6 +6,7 @@ module Metropoli
   module ClassMethods
     
     def belongs_to_metropoli
+      self.belongs_to :city, :class_name => 'Metropoli::City'
       send :include, InstanceMethods
     end
     
