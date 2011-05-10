3ccaaffe43dfd1165b06578d7dd8b45c7e32deb7
Macario Ortega <macarui@gmail.com>
2011-05-09 22:14:06 -0500

    small dryness refactoring in controller

--- app/controllers/metropoli_controller.rb
+++ app/controllers/metropoli_controller.rb
@@ -3,14 +3,19 @@ class MetropoliController < ActionController::Metal
   include ActionController::Rendering
 
   def cities
-    render :text => city_class.autocomplete(params[:q]).limit(autocomplete_limit).to_json, :content_type => "application/json"
+    render_autocomplete city_class
   end
 
   def states
-    render :text => state_class.autocomplete(params[:q]).limit(autocomplete_limit).to_json, :content_type => "application/json"
+    render_autocomplete state_class
   end
 
   def countries
-    render :text => country_class.autocomplete(params[:q]).limit(autocomplete_limit).to_json, :content_type => "application/json"
+    render_autocomplete country_class
+  end
+
+  private 
+  def render_autocomplete klass
+    render :text => klass.autocomplete(params[:q]).limit(autocomplete_limit).to_json, :content_type => "application/json"
   end
 end
