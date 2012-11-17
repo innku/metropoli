2d443be7946393efce6e945b4659f399539cbe75
e3matheus <e3matheus@gmail.com>
2012-11-17 03:07:09 -0600

    [Refactor] Removed duplication of metropoli api calls

--- lib/metropoli/api/api.rb
+++ lib/metropoli/api/api.rb
@@ -4,35 +4,29 @@ module Metropoli
   class Api < Sinatra::Base
     include ConfigurationHelper
   
+    helpers do
+      def autocomplete(model, search_param)
+        model.autocomplete(search_param).limit(autocomplete_limit)
+      end
+    end
+
     get '/metropoli/cities.json' do
-      @cities = city_class.autocomplete(params[:q])
-      @cities = @cities.merge(city_class.limit(autocomplete_limit))
-      @cities.to_json
+      autocomplete(city_class, params[:q]).to_json
     end
   
     get '/metropoli/states.json' do 
-      @states = state_class.autocomplete(params[:q])
-      @states = @states.merge(state_class.limit(autocomplete_limit))
-      @states.to_json
+      autocomplete(state_class, params[:q]).to_json
     end
   
     get '/metropoli/countries.json' do
-      @countries = country_class.autocomplete(params[:q])
-      @countries = @countries.merge(country_class.limit(autocomplete_limit))
-      @countries.to_json
+      autocomplete(country_class, params[:q]).to_json
     end
     
     get '/metropoli/cities_and_countries.json' do
-      @countries = country_class.autocomplete(params[:q])
-      @countries = @countries.merge(country_class.limit(autocomplete_limit))
+      countries = autocomplete(country_class, params[:q])
+      cities = autocomplete(city_class, params[:q])
 
-      @cities = city_class.autocomplete(params[:q])
-      @cities = @cities.merge(city_class.limit(autocomplete_limit))
-
-      {
-        'countries' => @countries,
-        'cities' => @cities
-      }.to_json
+      { 'countries' => countries, 'cities' => cities }.to_json
     end
   end
 end
